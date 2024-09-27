89c2b91aff7f9a8eafcb66e24063e07d47150366
Ali Rahimi <ali.rahimi@gandom.co>
2024-09-27 14:47:10 +0330

    add bootstrap

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div>
-      <Message />
+      <ListGroup />
     </div>
   );
 }
---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
