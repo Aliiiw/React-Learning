0ba82ab4d983a036380e1195e703313c7593be7e
Ali Rahimi <ali.rahimi@gandom.co>
2024-09-27 14:51:16 +0330

    loop items

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,13 +1,20 @@
 function ListGroup() {
+  const items = [
+    "New York",
+    "San Francisco",
+    "Tokyo",
+    "London",
+    "Paris",
+    "Shiraz"
+  ];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
