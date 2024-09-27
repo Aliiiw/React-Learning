ae64fa2e644a1d2e31180c42364a8a9b183019d6
Ali Rahimi <ali.rahimi@gandom.co>
2024-09-27 15:14:09 +0330

    props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,9 +4,18 @@ import "./App.css";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = [
+    "New York",
+    "San Francisco",
+    "Tokyo",
+    "London",
+    "Paris",
+    "Shiraz"
+  ];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,21 +1,16 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = [
-    "New York",
-    "San Francisco",
-    "Tokyo",
-    "London",
-    "Paris",
-    "Shiraz"
-  ];
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
 
-  // state Hook
+function ListGroup({ items, heading }: ListGroupProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
