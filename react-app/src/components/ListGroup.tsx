cdfcf87191701c0f291782c1547313d83cbedebd
Ali Rahimi <ali.rahimi@gandom.co>
2024-09-27 15:10:09 +0330

    useStates

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,4 +1,5 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+
 function ListGroup() {
   let items = [
     "New York",
@@ -9,11 +10,8 @@ function ListGroup() {
     "Shiraz"
   ];
 
-  const getMessage = () => {
-    items.length === 0 ? <p>No item found</p> : null;
-  };
-
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // state Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -21,7 +19,17 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : '"list-group-item'
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
